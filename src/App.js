29c1c87a9cd97f0e420e3f10d4fb46b5ac319811
Devesh-19 <devdeveshsingh@gmail.com>
2021-06-25 08:20:08 +0530

    Create UI for the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 import "./App.css";
 
@@ -6,6 +10,12 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExpenses />
+				<TransactionList />
+				<AddTransaction />
+			</div>
 		</div>
 	);
 }
